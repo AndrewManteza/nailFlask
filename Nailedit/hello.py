907633cafc8bc0c41e9f51f356f5262897b2a6c9
Andrew Manteza <manteza14@gmail.com>
2024-10-21 16:53:01 +0800

    added pics and another route

--- Nailedit/hello.py
+++ Nailedit/hello.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 
 @app.route("/home")
+
+
 def home_page():
     return render_template('home.html')
+
+@app.route("/pressons")
+def pressons_page():
+    return render_template('pressons.html')
