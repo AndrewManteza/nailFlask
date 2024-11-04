4925a290ecb01e2153d1375c887a65b2cf2861a8
Andrew Manteza <manteza14@gmail.com>
2024-11-04 18:16:35 +0800

    tried to fix lightbox

--- Nailedit/hello.py
+++ Nailedit/hello.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 @app.route("/home")
 
 
+
 def home_page():
     return render_template('home.html')
 
 @app.route("/pressons")
 def pressons_page():
     return render_template('pressons.html')
+
+@app.route("/lightboxtest")
+def lightboxtest_page():
+    return render_template('lightboxtest.html')
